b4b8c329ccadc7f9b41a6dd691bc6e5121f0d901
leebib1 <leeunji9807@gmail.com>
2024-04-14 18:34:42 +0900

    스프링MVC2 Section2 - 타임리프 : 스프링 통합과 폼

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,21 +1,52 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions") //모델에 항상 담겨있음.
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>(); //LinkedHashMap을 사용하면 등록 순서가 보장된다.
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values(); //ENUM의 모든 정보를 배열로 반환
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() { //해당 방법을 사용하면 컨트롤러가 호출될 때마다 사용되기 때문에 실제 코드에서는 미리 생성해두고 재사용하는 방법이 더 효율적이다.
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -32,12 +63,20 @@ public class FormItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        log.info("item.open={}", item.getOpen()); //값이 잘 넘어오는지 확인. log를 사용하려면 @Slf4j 추가
+        //HTML에서 체크 박스를 선택하지 않고 폼을 전송하면 open 이라는 필드 자체가 서버로 전송되지 않는다
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
+        
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
