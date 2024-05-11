f862150ab14dd50198fada2ad134b19c1364d9df
leebib1 <leeunji9807@gmail.com>
2024-05-12 06:08:11 +0900

    스프링 MVC2 Section 5 - 검증2 - Bean Validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,21 +1,37 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
+
+//    @NotBlank(message = "공백X", groups = {UpdateCheck.class, SaveCheck.class}) //빈값+공백만 있는 경우 허용하지 않는다.
     private String itemName;
+
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class}) //Null 값을 허용하지 않는다.
+//    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class}) //지정 범위 안의 값이어야 한다.
     private Integer price;
+
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) //최대 지정값까지만 허용한다. 등록시에만 적용
     private Integer quantity;
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+//    public Item() {
+//    }
+//
+//    public Item(String itemName, Integer price, Integer quantity) {
+//        this.itemName = itemName;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
 }
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -37,20 +37,20 @@ public class ValidationItemControllerV2 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v1/items";
+        return "validation/v2/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v1/item";
+        return "validation/v2/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "validation/v1/addForm";
+        return "validation/v2/addForm";
     }
 
 //    @PostMapping("/add")
@@ -83,7 +83,7 @@ public class ValidationItemControllerV2 {
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v1/addForm";
+            return "validation/v2/addForm";
         }
 
         //검증 성공 로직
@@ -241,7 +241,7 @@ public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, 
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v1/editForm";
+        return "validation/v2/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
