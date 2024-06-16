06469527b4503bbdba527475b5d180a4c9297cdb
leebib1 <leeunji9807@gmail.com>
2024-06-16 19:56:52 +0900

    스프링MVC2 - Section10 스프링 타입 컨버터

---
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,28 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        //아래 컨버터를 등록하지 않아도 스프링이 등록한 기본 컨버터들이 동작한다. 따로 등록해주면 따로 등록한 컨버터가 우선권을 가짐.
+        //우선 순위 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
+
+    }
+}
